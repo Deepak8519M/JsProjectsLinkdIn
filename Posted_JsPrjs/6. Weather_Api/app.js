160b7ca6ac028e7b25a34edf14d8b5cd07270504
Deepak8519M <157715193+Deepak8519M@users.noreply.github.com>
2025-02-16 21:12:43 +0530

    Update app.js

--- Posted_JsPrjs/6. Weather_Api/app.js
+++ Posted_JsPrjs/6. Weather_Api/app.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "57614e5a601f9d4dc2b961a7c15f5d85";
+const apiKey = "add yours";
 
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
